4f0c55c9261eba865e722f2e1b68d38445b9ffeb
emanoelCarvalho <ehcs@discente.ifpe.edu.br>
2025-01-11 21:23:26 -0300

    feat: add CNPJ validation and retrieval endpoint in CnpjController

--- src/cnpj/cnpj.controller.ts
+++ src/cnpj/cnpj.controller.ts
@@ -1,8 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, HttpException } from '@nestjs/common';
 import { CnpjService } from './cnpj.service';
 
 @Controller('cnpj')
 export class CnpjController {
-  constructor(private readonly cnpjService: CnpjService) {}
+  constructor(private cnpjService: CnpjService) {}
 
+  @Get(':cnpj')
+  async getCnpjInfo(@Param('cnpj') cnpj: string) {
+    if (!/^\d{14}$/.test(cnpj)) {
+      throw new HttpException('CNPJ inválido!', 400);
+    }
+    return this.cnpjService.getCnpjInfo(cnpj);
+  }
 }
