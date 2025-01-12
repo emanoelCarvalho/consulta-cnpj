d637cf190c422a5a5c7e912913b961027937c914
emanoelCarvalho <ehcs@discente.ifpe.edu.br>
2025-01-11 21:21:15 -0300

    feat: enhance CnpjService to fetch CNPJ info from external API with error handling

--- src/cnpj/cnpj.service.ts
+++ src/cnpj/cnpj.service.ts
@@ -1,7 +1,32 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import axios from 'axios';
 
 
 @Injectable()
 export class CnpjService {
-  
+  private  apiUrl: string;
+  private  apiKey: string; 
+
+  constructor(config: ConfigService) {
+    this.apiUrl = config.get<string>('API_URL');
+    this.apiKey = config.get<string>('API_KEY');
+  }
+
+  async getCnpjInfo(cnpj: string) {
+    try {
+      const response = await axios.get(`${this.apiUrl}${cnpj}`, {
+        headers: {
+          Authorization: `Bearer ${this.apiKey}`, 
+        }, 
+      }); 
+
+      return response.data;
+    } catch (error) {
+      throw new HttpException(
+        "Error ao consultar o CNPJ", 
+        HttpStatus.BAD_REQUEST
+      )
+    }
+  }
 }
