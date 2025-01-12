e7e74872a819defb83e604513d224885aa51b114
emanoelCarvalho <ehcs@discente.ifpe.edu.br>
2025-01-12 16:56:23 -0300

    :sparkles: feat: utilize EnvService for dynamic port configuration in main application bootstrap

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { EnvService } from './env/env.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  
+  const config = app.get(EnvService); 
+  const PORT = config.getPort;
+  
+  await app.listen(PORT );
 }
 bootstrap();
