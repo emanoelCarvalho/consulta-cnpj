1051b80faa9480607ca0df15a4d3c149c9f33e82
emanoelCarvalho <ehcs@discente.ifpe.edu.br>
2025-01-12 16:32:42 -0300

    :sparkles: feat: add EnvModule for global configuration management

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { CnpjModule } from './cnpj/cnpj.module';
+import { EnvModule } from './env/env.module';
 
 @Module({
-  imports: [CnpjModule, ],
+  imports: [CnpjModule, EnvModule],
   controllers: [],
   providers: [],
 })
