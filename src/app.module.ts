a6055e3f5d845bb112b87967e9fac34829918443
emanoelCarvalho <ehcs@discente.ifpe.edu.br>
2025-01-12 16:31:27 -0300

    :fire: refactor: remove ConfigModule and clean up app.module.ts imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,8 @@
 import { Module } from '@nestjs/common';
 import { CnpjModule } from './cnpj/cnpj.module';
-import { ConfigModule } from './config/config.module';
 
 @Module({
-  imports: [CnpjModule, ConfigModule],
+  imports: [CnpjModule, ],
   controllers: [],
   providers: [],
 })
